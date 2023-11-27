9b71fbd2416ed2141d72164897712f3614f68eb5
范剑虹 <123@abc.com>
2023-11-27 23:10:33 +0800

    Home首页静态组件的拆分

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,11 +1,27 @@
 <template>
-    <TypeNav></TypeNav>
+    <div>
+        <TypeNav></TypeNav>
+        <ListContainer></ListContainer>
+        <Recommend></Recommend>
+        <Rank></Rank>
+        <Like></Like>
+        <Floor></Floor>
+        <Floor></Floor>
+        <brand></brand>
+    </div>
 </template>
   
   <script>
+import ListContainer from "@/pages/Home/ListContainer";
+import Recommend from "@/pages/Home/Recommend";
+import Rank from "@/pages/Home/Rank";
+import Like from "@/pages/Home/Like";
+import Floor from "@/pages/Home/Floor";
+import Brand from "@/pages/Home/Brand";
 
 export default {
     name: "",
+    components: { ListContainer, Recommend, Rank, Like, Floor, Brand },
 };
 </script>
   
