bafeb284d002cfb2f59b6574856205af225538d3
范剑虹 <123@abc.com>
2023-11-29 23:07:00 +0800

    1

--- src/api/request.js
+++ src/api/request.js
@@ -12,7 +12,6 @@ const requests = axios.create({
 })
 
 // 请求拦截器：在发请求之前，请求拦截器可以检测到，可以在请求发出去之前做一些事情
-
 requests.interceptors.request.use((config) => {
     // config：配置对象，对象里有一个属性很重要，headers请求头
     return config;
